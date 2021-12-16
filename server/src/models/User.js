061cb1f69c62e47cda3fe4e6a9fee5c904177f2c
jblici <juambif@gmail.com>
2021-12-16 11:00:05 -0300

    first commit

---
+++ server/src/models/User.js
@@ -0,0 +1,24 @@
+const { DataTypes, UUID } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+  });
+};
+
+
